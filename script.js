cb6a6f2d5b984ed86e2e2bfa7d4f5f7099b68ad7
Rishi <nilam.bardolia@gmail.com>
2024-12-12 15:58:02 +0530

    Initial commit: Add basic HTML, CSS, and JavaScript files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeText').addEventListener('click', function() {
+    const displayText = document.getElementById('display-text');
+    displayText.textContent = 'Hello! You clicked the button!';
+});
